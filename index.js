4337a8265977397829b049512fed6e807a3b3d24
Erin Yant <ghosrider383@gmail.com>
2024-04-04 18:55:58 -0600

    completed

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+let num1 = 2;
+let num2 = 31;
+    let multiply = num1 * num2
 
-// Write your code here
+
+let randomInteger = Math.floor(Math.random() * (Number.MAX_SAFE_INTEGER - 1)) + 1;
+let random = randomInteger;
+
+let num3 = 14;
+let num4 = 5;
+
+let mod = num3 % num4;
+
+const set = [10, 5, 20, 15, 8];
+let max = set[0];
+
+for (let i = 1; i < set.length; i++) {
+  if (set[i] > max) {
+    max = set[i];
+  }
+}
